a805adbf42a5d274a225d4c6c5a04918f49069b3
Keiron McCammon <mail@keironmccammon.com>
2011-06-18 11:46:44 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RentalBookinsApp::Application.config.secret_token = '02c723995446650622db5d0dbb8022e30ff88257c13e29dca7bc0811b5cfabbd245c9aa4f9a8659f7fd8d5a796f9f86dcde72153a6d662d43ef10814dd3f21bc'
